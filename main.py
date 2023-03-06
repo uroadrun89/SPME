90945f39da662ecbc1a57cd7384df6ebcea8f7eb
freshremix <90133873+freshremix@users.noreply.github.com>
2023-03-06 11:35:55 +0330

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def get_single_song(update, context):
     context.bot.send_message(chat_id=chat_id, text="Fetching...")
 
     if config["SPOTDL_DOWNLOADER"]:
-        os.system(f'spotdl download {url} --threads 4 --format mp3 --bitrate 320k --lyrics genius')
+        os.system(f'spotdl download {url} --threads 3 --format mp3 --bitrate 320k --lyrics genius')
     elif config["SPOTIFYDL_DOWNLOADER"]:
         os.system(f'spotifydl {url}')
     else:
@@ -90,6 +90,6 @@ def authenticate(update, context):
 handler = MessageHandler(Filters.text, get_single_song_handler)
 dispatcher.add_handler(handler=handler)
 
-POLLING_INTERVAL = 8.8
+POLLING_INTERVAL = 2.8
 updater.start_polling(poll_interval=POLLING_INTERVAL)
 updater.idle()
