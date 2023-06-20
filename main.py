9f4f81fcf05515b45d85bf9b911ce0141ac12421
freshremix <90133873+freshremix@users.noreply.github.com>
2023-06-20 15:53:56 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,111 +1,97 @@
+import logging
 import os
 import json
-import spotipy
-import spotipy.util as util
-import telebot
-import requests
-from dotenv import load_dotenv
-from threading import Thread
-import ffmpeg
+from dotenv import dotenv_values
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-load_dotenv()
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Load configuration from config.json
-with open('config.json') as f:
-    config = json.load(f)
+class Config:
+    def __init__(self):
+        self.load_config()
 
-# Load environment variables from .env
-SPOTIFY_USERNAME = os.getenv('SPOTIFY_USERNAME')
-SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID') or config['spotify_client_id']
-SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET') or config['spotify_client_secret']
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN') or config['telegram_bot_token']
+    def load_config(self):
+        try:
+            token = dotenv_values(".env")["TELEGRAM_TOKEN"]
+        except Exception as e:
+            logger.error(f"Failed to load token from .env file: {e}")
+            token = os.environ.get('TELEGRAM_TOKEN')
+            if token is None:
+                logger.error("Telegram token not found. Make sure to set TELEGRAM_TOKEN environment variable.")
+                raise ValueError("Telegram token not found.")
+        self.token = token
+        self.auth_enabled = False  # Change to True if authentication is required
+        self.auth_password = "your_password"  # Set the desired authentication password
+        self.auth_users = []  # List of authorized user chat IDs
 
-# Authentication credentials from config.json
-AUTHENTICATION_METHOD = config['authentication']['method']
-AUTHENTICATION_USERNAME = config['authentication']['username']
-AUTHENTICATION_PASSWORD = config['authentication']['password']
+config = Config()
 
-# Authenticate with Spotify
-scope = 'user-library-read'
-token = util.prompt_for_user_token(SPOTIFY_USERNAME, scope, client_id=SPOTIFY_CLIENT_ID,
-                                   client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri='http://localhost/')
+def authenticate(func):
+    def wrapper(update, context):
+        chat_id = update.effective_chat.id
+        if config.auth_enabled:
+            if chat_id not in config.auth_users:
+                context.bot.send_message(chat_id=chat_id, text="⚠️ This bot is personal and you are not signed in. Please enter the password to sign in. If you don't know it, contact the bot owner.")
+                return
+        return func(update, context)
+    return wrapper
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-else:
-    raise ValueError('Failed to authenticate with Spotify.')
+def start(update, context):
+    chat_id = update.effective_chat.id
+    context.bot.send_message(chat_id=chat_id, text="Welcome to the Song Downloader Bot!")
 
-# Initialize Telegram bot
-bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+def get_single_song(update, context):
+    chat_id = update.effective_chat.id
+    message_id = update.effective_message.message_id
+    username = update.effective_chat.username
+    logger.info(f'Starting song download. Chat ID: {chat_id}, Message ID: {message_id}, Username: {username}')
 
+    url = update.effective_message.text.strip()
 
-def download_file(url, filename):
-    response = requests.get(url, stream=True)
-    with open(filename, 'wb') as f:
-        for chunk in response.iter_content(chunk_size=1024):
-            if chunk:
-                f.write(chunk)
-    return filename
+    download_dir = f".temp{message_id}{chat_id}"
+    os.makedirs(download_dir, exist_ok=True)
+    os.chdir(download_dir)
 
+    logger.info('Downloading song...')
+    context.bot.send_message(chat_id=chat_id, text="Fetching...")
 
-def convert_to_mp3(input_file, output_file):
-    ffmpeg.input(input_file).output(output_file, audio_bitrate='320k', threads=12).run()
+    if url.startswith(("http://", "https://")):
+        os.system(f'spotdl download "{url}" --threads 12 --format mp3 --bitrate 320k --lyrics genius')
 
-
-def delete_temp_folder(folder_path):
-    for file_name in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file_name)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-    os.rmdir(folder_path)
-
-
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    bot.reply_to(message, "Welcome to the Spotify Downloader Bot! Send me the name of an artist, playlist, album, "
-                          "or track, and I'll send you the download link.")
-
-
-@bot.message_handler(func=lambda message: True)
-def handle_message(message):
-    query = message.text
-    chat_id = message.chat.id
-
-    # Create a temporary folder to store downloaded files
-    temp_folder = 'temp'
-    if not os.path.exists(temp_folder):
-        os.makedirs(temp_folder)
-
-    try:
-        search_result = sp.search(q=query, limit=1, type='track')
-        track = search_result['tracks']['items'][0]
-        track_name = track['name']
-        track_artists = ', '.join([artist['name'] for artist in track['artists']])
-        track_url = track['preview_url']
-
-        if track_url:
-            download_thread = Thread(target=download_file, args=(track_url, f'{temp_folder}/{track_name}.ogg'))
-            download_thread.start()
-
-            bot.send_message(chat_id, f'Downloading: {track_name} - {track_artists}')
-            bot.send_chat_action(chat_id, 'typing')
-
-            download_thread.join()
-
-            mp3_filename = f'{temp_folder}/{track_name}.mp3'
-            convert_to_mp3(f'{temp_folder}/{track_name}.ogg', mp3_filename)
-
-            audio_file = open(mp3_filename, 'rb')
-            bot.send_audio(chat_id, audio_file)
-
-            # Cleanup - Delete the temporary folder
-            delete_temp_folder(temp_folder)
+        logger.info('Sending song to user...')
+        sent = 0
+        files = [file for file in os.listdir(".") if file.endswith(".mp3")]
+        if files:
+            for file in files:
+                context.bot.send_audio(chat_id=chat_id, audio=open(file, 'rb'), timeout=18000)
+                sent += 1
+            logger.info(f'Sent {sent} audio file(s) to user.')
         else:
-            bot.send_message(chat_id, f"No preview available for: {track_name} - {track_artists}")
-    except IndexError:
-        bot.send_message(chat_id, f"No results found for: {query}")
-    except Exception as e:
-        bot.send_message(chat_id, f"An error occurred: {str(e)}")
+            context.bot.send_message(chat_id=chat_id, text="Unable to find the requested song.")
+            logger.warning('No audio file found after download.')
+    else:
+        context.bot.send_message(chat_id=chat_id, text="Invalid URL. Please provide a valid song URL.")
+        logger.warning('Invalid URL provided.')
 
+    os.chdir('..')
+    os.system(f'rm -rf {download_dir}')
 
-bot.polling()
+def main():
+    updater = Updater(token=config.token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # Handlers
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    song_handler = MessageHandler(Filters.text & (~Filters.command), get_single_song)
+    dispatcher.add_handler(song_handler)
+
+    # Start the bot
+    updater.start_polling()
+    logger.info('Bot started.')
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
